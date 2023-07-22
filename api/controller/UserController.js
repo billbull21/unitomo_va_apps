73a815efb17658dae7e5c53e3f4ba7bf64d2431a
khabib <oemampedia@gmail.com>
2023-07-22 18:54:36 +0700

    edit endpoint get all users

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -17,7 +17,7 @@ exports.get = async function (req, res) {
   */
   try {
     let users = await knex.raw(
-      `select u.*, mp.namaprodi from users u join m_prodi mp on mp.kdprodi = u.prodi where u.isAdmin=false AND u.status=1`
+      `select u.*, mp.namaprodi from users u join m_prodi mp on mp.kdprodi = u.prodi where u."isAdmin"=false AND u.status=1`
     );
     if (users.length > 0 && req.user.isAdmin) {
       return res.status(200).json({
